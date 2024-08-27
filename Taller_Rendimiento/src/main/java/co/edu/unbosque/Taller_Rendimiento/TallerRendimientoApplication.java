f66137c5a692d77e36d62161a9d15a3b1e449339
ManuelTerreros <114838362+ManuelTerreros@users.noreply.github.com>
2024-08-27 17:40:37 -0500

    Add files via upload

---
+++ Taller_Rendimiento/src/main/java/co/edu/unbosque/Taller_Rendimiento/TallerRendimientoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.Taller_Rendimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerRendimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerRendimientoApplication.class, args);
+	}
+
+}
