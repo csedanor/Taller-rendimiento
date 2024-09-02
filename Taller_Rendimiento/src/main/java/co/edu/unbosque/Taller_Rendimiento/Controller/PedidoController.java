c83c7b6a12be2dd6c066fe0c47bea9f985aea68d
csedanor <csedano@unbosque.edu.co>
2024-09-01 22:56:13 -0500

    PedidoService y PedidoController

--- Taller_Rendimiento/src/main/java/co/edu/unbosque/Taller_Rendimiento/Controller/PedidoController.java
+++ Taller_Rendimiento/src/main/java/co/edu/unbosque/Taller_Rendimiento/Controller/PedidoController.java
@@ -3,8 +3,8 @@ package co.edu.unbosque.Taller_Rendimiento.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,21 +17,20 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @RestController
 @RequestMapping("/pedidos")
 public class PedidoController {
-	
-	@Autowired 
-	private PedidoService pedidoService;
-	
-	
+    
+    @Autowired 
+    private PedidoService pedidoService;
+
     @PostMapping("/procesar")
     public ResponseEntity<?> procesarPedido(@RequestBody PedidoDTO pedidoDTO, @RequestBody List<DetallePedidoDTO> detallesDTO) {
-        
-    	//Agegar el método que se vaya a usar para la inserción de pedido, teniendo en cuenta el detalle pedido
-    	
-        return ResponseEntity.ok("Hola Q ace");
+        try {
+            // Llamar al servicio para procesar el pedido y sus detalles
+            pedidoService.procesarPedido(pedidoDTO, detallesDTO);
+            // Responder con un mensaje de éxito o de error
+            return ResponseEntity.ok("Pedido procesado exitosamente");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                 .body("Error al procesar el pedido: " + e.getMessage());
+        }
     }
-
-
-    
-    
-
 }
--- Taller_Rendimiento/src/main/java/co/edu/unbosque/Taller_Rendimiento/Service/PedidoService.java
+++ Taller_Rendimiento/src/main/java/co/edu/unbosque/Taller_Rendimiento/Service/PedidoService.java
@@ -3,13 +3,32 @@ package co.edu.unbosque.Taller_Rendimiento.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.unbosque.Taller_Rendimiento.DTO.DetallePedidoDTO;
+import co.edu.unbosque.Taller_Rendimiento.DTO.PedidoDTO;
+import co.edu.unbosque.Taller_Rendimiento.Entidades.PedidoEntity;
 import co.edu.unbosque.Taller_Rendimiento.Repository.PedidoRepository;
+
+import java.util.List;
 
 @Service
 public class PedidoService {
-	@Autowired
-	private PedidoRepository pedidoRepo;
-	
-	
+    @Autowired
+    private PedidoRepository pedidoRepo;
 
+    @Autowired
+    private DetallePedidoService detallePedidoService;
+
+    public void procesarPedido(PedidoDTO pedidoDTO, List<DetallePedidoDTO> detallesDTO) {
+        // Convertir PedidoDTO a PedidoEntity
+        PedidoEntity pedidoEntity = new PedidoEntity();
+        pedidoEntity.setIdCliente(pedidoDTO.getIdCliente());
+        pedidoEntity.setTotal(pedidoDTO.getTotal());
+        pedidoEntity.setEstado(pedidoDTO.getEstado());
+        // Guardar el pedido en la base de datos
+        PedidoEntity pedidoGuardado = pedidoRepo.save(pedidoEntity);
+        // Procesar y guardar cada detalle del pedido
+        for (DetallePedidoDTO detalleDTO : detallesDTO) {
+            detallePedidoService.crearDetalle(pedidoGuardado, detalleDTO);
+        }
+    }
 }
