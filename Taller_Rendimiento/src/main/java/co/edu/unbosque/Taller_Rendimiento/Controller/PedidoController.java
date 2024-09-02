8a2aa2dccdcceee0c844363d4732fcf7c46a3d2c
Valeb22 <105802946+Valeb22@users.noreply.github.com>
2024-09-02 00:07:13 -0500

    Add files via upload

--- Taller_Rendimiento/src/main/java/co/edu/unbosque/Taller_Rendimiento/Controller/PedidoController.java
+++ Taller_Rendimiento/src/main/java/co/edu/unbosque/Taller_Rendimiento/Controller/PedidoController.java
@@ -1,36 +1,39 @@
 package co.edu.unbosque.Taller_Rendimiento.Controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import co.edu.unbosque.Taller_Rendimiento.DTO.DetallePedidoDTO;
 import co.edu.unbosque.Taller_Rendimiento.DTO.PedidoDTO;
 import co.edu.unbosque.Taller_Rendimiento.Service.PedidoService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
-@RequestMapping("/pedidos")
+@RequestMapping("/api/pedidos")
 public class PedidoController {
-    
-    @Autowired 
+
+    @Autowired
     private PedidoService pedidoService;
 
-    @PostMapping("/procesar")
-    public ResponseEntity<?> procesarPedido(@RequestBody PedidoDTO pedidoDTO, @RequestBody List<DetallePedidoDTO> detallesDTO) {
+    @PostMapping("/crear")
+    public ResponseEntity<PedidoDTO> crearPedido(@RequestParam int idCliente, @RequestParam float total) {
+        PedidoDTO pedidoCreacionDTO = new PedidoDTO(idCliente, total);
         try {
-            // Llamar al servicio para procesar el pedido y sus detalles
-            pedidoService.procesarPedido(pedidoDTO, detallesDTO);
-            // Responder con un mensaje de éxito o de error
-            return ResponseEntity.ok("Pedido procesado exitosamente");
+            PedidoDTO pedidoDTO = pedidoService.crearPedido(pedidoCreacionDTO);
+            return new ResponseEntity<>(pedidoDTO, HttpStatus.CREATED);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                 .body("Error al procesar el pedido: " + e.getMessage());
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @PutMapping("/procesar/{idPedido}")
+    public ResponseEntity<PedidoDTO> procesarPedido(@PathVariable int idPedido) {
+        try {
+            PedidoDTO pedidoDTO = pedidoService.procesarPedido(idPedido);
+            return ResponseEntity.ok(pedidoDTO);
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body(null); // Puedes ajustar el código de estado según el error
         }
     }
 }
