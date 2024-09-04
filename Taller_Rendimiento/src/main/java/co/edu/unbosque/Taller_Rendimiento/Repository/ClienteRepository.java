00742151996f59aae51b060e64e67ce88040aa97
Valeb22 <105802946+Valeb22@users.noreply.github.com>
2024-09-03 23:26:12 -0500

    Delete Taller_Rendimiento directory

--- Taller_Rendimiento/src/main/java/co/edu/unbosque/Taller_Rendimiento/Repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.Taller_Rendimiento.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.unbosque.Taller_Rendimiento.Entidades.ClienteEntity;
-
-public interface ClienteRepository extends JpaRepository<ClienteEntity, Integer>{
-
-}
